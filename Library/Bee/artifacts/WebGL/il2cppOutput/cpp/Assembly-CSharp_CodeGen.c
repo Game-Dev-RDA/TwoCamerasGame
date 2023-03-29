4ed7df6c2a65f5e82d3a95d0a149e94a08291d77
Reut Hadad <reuthadad123@gmail.com>
2023-03-29 22:29:52 +0300

    add two cameras game

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,57 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ClickMover::OnEnable()
+extern void ClickMover_OnEnable_m121E7A42E58075A89FAAEEF164A28D8842EA6B8D (void);
+// 0x00000002 System.Void ClickMover::OnDisable()
+extern void ClickMover_OnDisable_m3C4E4E3C1597DEB824F1F54859573F517FFDE8A9 (void);
+// 0x00000003 System.Void ClickMover::Update()
+extern void ClickMover_Update_m7EE59F1B0577B17E169126D3341C4BA594DF094A (void);
+// 0x00000004 System.Void ClickMover::.ctor()
+extern void ClickMover__ctor_mBB89F90DC54E436E5290F1F843C65DEBD540FA94 (void);
+static Il2CppMethodPointer s_methodPointers[4] = 
+{
+	ClickMover_OnEnable_m121E7A42E58075A89FAAEEF164A28D8842EA6B8D,
+	ClickMover_OnDisable_m3C4E4E3C1597DEB824F1F54859573F517FFDE8A9,
+	ClickMover_Update_m7EE59F1B0577B17E169126D3341C4BA594DF094A,
+	ClickMover__ctor_mBB89F90DC54E436E5290F1F843C65DEBD540FA94,
+};
+static const int32_t s_InvokerIndices[4] = 
+{
+	4197,
+	4197,
+	4197,
+	4197,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	4,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TwoCamerasGame
+
+Link to the game: https://reut212.itch.io/sample-of-two-cameras-game
